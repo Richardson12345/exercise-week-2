068376ee269640a6e54bb5f54349c8dd62df25ad
unknown <chuddywarrior@gmail.com>
2018-03-31 00:38:07 +0700

    first commit

---
+++ Menggunakan-Switch-Case-homework.js
@@ -0,0 +1,19 @@
+var tahun = '2018'
+var hari = '11'
+var bulan = 4;
+switch(bulan) {
+      case 1:   { console.log(hari + ' ' + 'January' + ' ' + tahun); break; }
+      case 2:   { console.log(hari + ' ' + 'February' + ' ' + tahun); break; }
+      case 3:   { console.log(hari + ' ' + 'March' + ' ' + tahun); break; }
+      case 4:   { console.log(hari + ' ' + 'April'+ ' ' + tahun); break; }
+      case 5:   { console.log(hari + ' ' + 'May' + ' ' + tahun); break; }
+      case 6:   { console.log(hari + ' ' + 'june' + ' ' + tahun); break; }
+      case 7:   { console.log(hari + ' ' + 'July' + ' ' + tahun); break; }
+      case 8:   { console.log(hari + ' ' + 'August' + ' ' + tahun); break; }
+      case 9:   { console.log(hari + ' ' + 'september' + ' ' + tahun); break; }
+      case 10:  { console.log(hari + ' ' + 'October' + ' ' + tahun); break; }
+      case 11:  { console.log(hari + ' ' + 'November' + ' ' + tahun); break; }
+      case 12:  { console.log(hari + ' ' + 'December' + ' ' + tahun); break; }
+  
+  default:  { console.log('Insert month from 1-12!'); }
+}
