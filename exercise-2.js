e0cb574dfc181889de6b74d6c41d1a24d04bbb81
Richardson12345 <37545118+Richardson12345@users.noreply.github.com>
2018-04-30 00:55:55 +0700

    Add files via upload

---
+++ exercise-2.js
@@ -0,0 +1,49 @@
+// Newton's Second Law
+
+// Algorithm:
+
+// 1. Create variable name resultant gaya
+// 2. Create variable named 'm' = 600
+// 3. Create variable named 'a' = 2
+// 4. Calculate resultant gaya by multiplying m * a
+// 5. Display the result
+
+// Pseudocode:
+
+// 1. STORE resultant gaya as empty variable
+// 2. STORE m as 600
+// 3. STORE a as 2
+// 4. COMPUTE resultant gaya  = m*a
+// 5. DISPLAY resultant gaya
+
+
+// Leap Year
+
+// Algorithm:
+
+// 1. List out years starting from 1582
+// 2. If year % 4 = 0, go to step 3, else go to step 6
+// 3. If year % 100 = 0 go to step 4, else go to step 5
+// 4. If year % 400 = 0 go to step 5, else go to step 6
+// 5. Print "This is a leap year (366 days)"
+// 6. Print "This is a normal year (365 days)"
+
+
+// Laundry Day
+
+// Algorithm:
+
+// 1. Create variable counter
+// 2. Set counter = 0
+// 3. If counter is < 20, go to step 4, else go to step 5
+// 4. Set counter = counter + 1, repeat step 3
+// 5. Display "Washing Machine is on"
+
+
+// Check nails
+
+// Algorithm:
+
+// 1. Create variable student starting with 0 students
+// 2. As long as students < 40, repeat step 3
+// 3. Check each student & if student has long nails, show "punish the student", else show "praise the student"
